4ecb076507a417091cb6276a0a3cc57a4657fa69
Javiermd <javiermardom@gmail.com>
2024-02-05 10:43:27 +0100

    05/02

---
+++ Assets/Scripts/Exercise01.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Exercise01 : MonoBehaviour
+{
+    /*int sum = 0;
+
+        for (int i = 2; i <= 100; i += 2)
+        {
+            sum += i;
+        }
+    Debug.Log("The sum of even numbers from 1 to 100 is: " + sum); */
+
+void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
