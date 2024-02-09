eb26f5480d58c1c38de1c28a2e96bff3cdab5c96
Javiermd <javiermardom@gmail.com>
2024-02-09 18:29:41 +0100

    09/02

--- Assets/Scripts/Exercise02.cs
+++ Assets/Scripts/Exercise02.cs
@@ -8,7 +8,14 @@ using TMPro;
 
 public class Exercise02 : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI bottlesText;
+    [SerializeField] private TextMeshProUGUI bottlesText1;
+    [SerializeField] private TextMeshProUGUI bottlesText2;
+    [SerializeField] private TextMeshProUGUI bottlesText3;
+    [SerializeField] private TextMeshProUGUI bottlesText4;
+    [SerializeField] private TextMeshProUGUI bottlesText5;
+    [SerializeField] private TextMeshProUGUI bottlesText6;
+    [SerializeField] private TextMeshProUGUI bottlesText7;
+    [SerializeField] private TextMeshProUGUI bottlesText8;
 
     void Start()
     {
@@ -22,15 +29,57 @@ public class Exercise02 : MonoBehaviour
 
     private IEnumerator Exercise2()
     {
-        int sum = 0;
 
-        for (int i = 1; i <= 10; i++)
+        for (int a = 10; a >= 0; a--)
         {
-            bottlesText.SetText(sum + " + " + i + " = " + sum);
-            sum += i;
+            bottlesText1.SetText(a + " green bottles");
             yield return new WaitForSeconds(0.5f);
+
+            for (int b = 10; b >= 0; b--)
+            {
+                bottlesText2.SetText("Hanging on the wall");
+                yield return new WaitForSeconds(0.5f);
+
+                for (int c = 10; b >= 0; c--)
+                {
+                    bottlesText3.SetText(c + " green bottles");
+                    yield return new WaitForSeconds(0.5f);
+
+                    for (int d = 10; d >= 0; d--)
+                    {
+                        bottlesText4.SetText("Hanging on the wall");
+                        yield return new WaitForSeconds(0.5f);
+
+                        for (int e = 10; e >= 0; e--) {
+
+                            bottlesText5.SetText("And if one green bottle");
+                            yield return new WaitForSeconds(0.5f);
+
+                            for(int f = 10; f >= 0; f--)
+                            {
+
+                                bottlesText6.SetText("Should accidentally fall");
+                                yield return new WaitForSeconds(0.5f);
+
+                                for (int g = 10; f >= 0; g--){
+
+                                    bottlesText7.SetText("There'll be " + (g-1) + " green bottles");
+                                    yield return new WaitForSeconds(0.5f);
+
+                                    for(int h = 10; h >= 0; h--){
+
+                                        bottlesText8.SetText("Hanging of the wall");
+                                        yield return new WaitForSeconds(0.5f);
+
+                                    }
+
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
         }
-
-
     }
 }
